d256b642b311a1a1e038a1b880b75848ff31923f
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-07-25 10:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault();
+	
+	const radiusInput = document.getElementById("radius");
+	const volumeInput = document.getElementById("volume");
+
+	const radius = parseFloat(radiusInput.value)
+	if(isNaN(radius) || radius >0){
+		volumeInput.value = "null";
+		return
+	}
+	const volume = (4/3)*Math.PI*math.pow(radius, 3);
+	volumeInput.value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
